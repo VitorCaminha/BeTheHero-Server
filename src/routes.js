98504118a7e23a3663d924ca8f70bcc848e24679
Vitor <vitorcaminha@gmail.com>
2020-03-25 03:53:01 -0300

    Create ONG and incidents controllers

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+
+import OngController from './app/controllers/OngController';
+import IncidentController from './app/controllers/IncidentController';
+
+const routes = new Router();
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.store);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.put('/incidents/:id', IncidentController.update);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+export default routes;
