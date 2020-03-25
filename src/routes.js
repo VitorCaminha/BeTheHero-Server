e6ea4730b08c50d20847d763902b5d5ded72f1e7
Vitor <vitorcaminha@gmail.com>
2020-03-25 04:25:48 -0300

    Create Profile and Session controllers

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
 import OngController from './app/controllers/OngController';
 import IncidentController from './app/controllers/IncidentController';
+import ProfileController from './app/controllers/ProfileController';
 
 const routes = new Router();
+
+routes.post('/sessions', SessionController.store);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.store);
@@ -12,5 +16,7 @@ routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.store);
 routes.put('/incidents/:id', IncidentController.update);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 export default routes;
